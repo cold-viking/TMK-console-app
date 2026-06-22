9c73ee8ddd8139d96a3a23f2a4047cc94524d5bf
cold-viking <vovashagapov@gmail.com>
2026-06-22 15:28:43 +0500

    iter2 - add part of Unit Tests

---
+++ SpaceObjects/SpaceObjects.Tests/Tests/Infrastructure/TestsDbFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceObjects.Data;
+
+namespace SpaceObjects.Tests.Infrastructure;
+
+public static class TestDbFactory
+{
+    public static AppDbContext Create()
+    {
+        DotNetEnv.Env.Load();
+
+        var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+        var port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+        var db = "space_db_test";
+        var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+        var connectionString =
+            $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(connectionString)
+            .Options;
+
+        var context = new AppDbContext(options);
+        
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        return context;
+    }
+}
